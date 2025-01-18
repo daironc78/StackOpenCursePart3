3a90464dbf1040a08bc2a853f8fdaff8a48ec899
Dairon <daironc78@gmail.com>
2025-01-18 16:45:30 -0500

    EjecicioFinalizado

--- index.jsx
+++ index.jsx
@@ -1,81 +1,121 @@
 const express = require("express");
 const app = express();
 
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(express.json());
+app.use(requestLogger);
+
 // HELP
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      people.length + 1
+    } people </p> <br /> ${new Date()}`
+  );
 });
 
 // GET ALL
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
 // GET FOR ID
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
+  const person = persons.find((person) => person.id === id);
 
-  if (note) {
-    response.json(note);
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-// DELETE FOR ID
-app.use(express.json());
-
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
+  persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
 });
 
 // SAVE NEW NOTE
 const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/persons", (request, response) => {
   const body = request.body;
+  let error = [];
+  if (!body.name || !body.number) {
+    console.log("name or number missing");
 
-  if (!body.content) {
+    if (!body.name) {
+      console.log("name missing");
+      error = error.concat({ name: "name missing" });
+    }
+
+    if (!body.number) {
+      console.log("number missing");
+      error = error.concat({ number: "number missing" });
+    }
+
     return response.status(400).json({
-      error: "content missing",
+      error: error,
     });
   }
 
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: error.concat({ name: "name must be unique" }),
+    });
+  }
+
+  const person = {
     id: generateId(),
+    name: body.name,
+    number: body.number,
   };
 
-  notes = notes.concat(note);
+  persons = persons.concat(person);
 
-  response.json(note);
+  response.json(person);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
